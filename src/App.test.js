89c92995a7e0cfef50f2eafaa4941c3be08d7493
Billy Arnold <billyarnoldd@gmail.com>
2024-05-05 15:50:43 +0100

    Extremely basic testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,14 @@
-test("Fake test", () => {
-  expect(true).toBeTruthy();
+import { total } from "./App";
+import { add } from "./add";
 
-  expect(false).not.toBeTruthy();
+jest.mock("./add", () => ({
+  add: jest.fn(() => 25),
+}));
+
+test("total", () => {
+  expect(total(5, 20)).toBe("$25");
+  expect(add).toHaveBeenCalledTimes(1);
+
+  add.mockImplementation(() => 30);
+  expect(total(10, 20)).toBe("$30");
 });
